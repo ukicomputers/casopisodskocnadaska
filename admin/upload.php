b05edeecebd013010ed01fa95ddd184e6dda7966
Uglješa <84191191+ukicomputers@users.noreply.github.com>
2022-10-21 18:07:05 +0200

    Delete admin directory

--- admin/upload.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $FileName=$_FILES['fuResume']['name'];
-    $TmpName=$_FILES['fuResume']['tmp_name'];
-
-    move_uploaded_file($TmpName,$FileName);
-
-    echo("Fajl uspesno kopiran na server");
-?>
